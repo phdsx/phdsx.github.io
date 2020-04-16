8c98318ec962f90e09b27c0070b83d1f6e86658b
phdsx <pp93314@163.com>
2020-04-17 00:31:25 +0800

    js更新

--- JS/scripts/main.js
+++ JS/scripts/main.js
@@ -1,2 +1,6 @@
 let myHeading =document.querySelector('h1');
-myHeading.textContent='Hello world!';
+myHeading.textContent='测试网页，无实际内容';
+
+document.querySelector('html').onclick=function{
+	alert('别点我，我怕疼！')
+}
